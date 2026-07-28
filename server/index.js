425a4478c084eb23c64834929353687c55d96163
Saurabh-Singh33 <singhsaurabh43431@gmail.com>
2026-07-28 11:30:56 +0530

    Made changes for room enter id

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
+const authRoutes = require('./routes/auth');
 const { createUser, getUserByUsername, createRoom, getRoom } = require('./db');
 
 const app = express();
